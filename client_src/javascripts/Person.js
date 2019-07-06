fdd38a35d2d881e097131375741ddbeb9a17b6e0
Hans-Christiaan Braun <hc.braun.93@gmail.com>
2019-07-06 11:40:41 +0200

    Removed any RDFa attributes.
    I decided to leave it for now, since it muddles up the code.
    Linked Data parsers can use the JSON-LD in the head for now to retrieve
    the data from the page, which is far easier than to parse the embedded RDFa.

--- client_src/javascripts/Person.js
+++ client_src/javascripts/Person.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
 function Person( person ) {
-
-    const id = person["@id"];
-    const context = person["@context"];
-    const type = person["@type"];
-
     return (
-        <div resource={id} vocab={context} typeof={type}>
-            <h1 property="name">{person.name}</h1>
+        <div>
+            <h1>{person.name}</h1>
             <dl>
                 <dt>Job title</dt>
-                <dd property="jobTitle">{person.jobTitle}</dd>
+                <dd>{person.jobTitle}</dd>
                 <dt>Email</dt>
-                <dd><a property="email" href={person.email}>{person.email.replace(/^mailto:/,"")}</a></dd>
+                <dd><a href={person.email}>{person.email.replace(/^mailto:/,"")}</a></dd>
             </dl>
         </div>
     );
