a6692dd70e2c9a72202b33a185613b24022a9157
Hans-Christiaan Braun <hc.braun.93@gmail.com>
2019-07-19 18:00:44 +0200

    Moved frontend source data to their own folder. Introduced admin client_src folder.

--- webpack.client.js
+++ webpack.client.js
@@ -8,7 +8,7 @@ const baseConfig = require( './webpack.common.js' );
 
 const config = {
   context: __dirname,
-  entry: [ './client_src/javascripts/index.js' ],
+  entry: [ './client_src/frontend/script/index.js' ],
   output: {
     path: path.join( __dirname, 'public', 'js' ),
     filename: 'client-bundle.js',
